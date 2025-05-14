2cab2303b139c488b89a333f25dd693dead531b7
miguelgallardof <miguel.gallardof@icloud.com>
2025-05-13 20:44:20 -0500

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,5 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({
-  site: 'https://miguelogf.github.io',
-  base: 'my-repo',
-});
+export default defineConfig({});
