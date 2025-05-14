54766ebb772efd8654c251cb4caee73435a72902
Miguel Gallardo <miguel.gallardof@icloud.com>
2025-05-13 20:15:27 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://miguelogf.github.io',
+  base: 'my-repo',
+});
